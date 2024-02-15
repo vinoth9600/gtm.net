5f6cccfc34b82a62aafcebb940a1c8184d0b823f
vinoth9600 <118962988+vinoth9600@users.noreply.github.com>
2024-02-15 16:45:10 +0530

    Update Universal.js

--- Universal.js
+++ Universal.js
@@ -1,6 +1,6 @@
 function sendResponse()
 {
-<script src='https://cdn-prod.securiti.ai/consent/form-consent-sdk.js'></script>
+                                <script src='https://cdn-prod.securiti.ai/consent/form-consent-sdk.js'></script>
                                 <script>window.addEventListener('load',
     FormConsentSDK.initialize({
         codeGenMetadata:{"formPropertyId":"8","formSelector":"[id*=\"145\"]","subjectIdentifiers":[{"name":"name","selector":"[name = \"name\"]","isPrimary":false},{"name":"Enter Email - ","selector":"[name = \"email\"]","isPrimary":true},{"name":"submit","selector":"[name = \"submit\"]","isPrimary":false}],"consentIdentifiers":[{"type":"checkbox","name":"Agree","selector":"[name = \"Agree\"]","propertyId":43150,"consentValue":"checked"}],"consentTrigger":{"action":"submit","selector":"[name = \"submit\"]"}},
